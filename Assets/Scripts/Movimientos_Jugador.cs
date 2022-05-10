0e2f69274b5e105acea1c4953d5e36828e78cc37
Andres Arbona <andres.trabajo90@gmail.com>
2022-05-09 22:02:09 -0300

    Movimi3ntos
    Movimientos jugador

--- Assets/Scripts/Movimientos_Jugador.cs
+++ Assets/Scripts/Movimientos_Jugador.cs
@@ -2,19 +2,71 @@ using UnityEngine;
 
 public class Movimientos_Jugador : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D Cuerpo;
-    [SerializeField] private float Velocidad;
+    [Header ("Variables de movilidad")]
+
+    [SerializeField] private float VelocidadMovimiento;
+
+    [Header("Layers")]
+    [SerializeField] private LayerMask SuperficieLayer;
+
+    private BoxCollider2D Colision;
+    private Rigidbody2D Cuerpo;
+
+    private float MovimientosJugador;
+    private bool PuedeSaltar = true;
+    private float ValorSalto = 0.0f;
+    private float TiempoX;
+    private float TiempoY;
 
     private void Awake()
     {
         Cuerpo = GetComponent<Rigidbody2D>();
+        Colision = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
-        Cuerpo.velocity = new Vector2(Input.GetAxis("Horizontal") * Velocidad, Cuerpo.velocity.y);
+        MovimientosJugador = Input.GetAxis("Horizontal");
 
-        if (Input.GetKey(KeyCode.Space))
-            Cuerpo.velocity = new Vector2(Cuerpo.velocity.x, Velocidad);
+        if (ValorSalto == 0.0f && EnSuelo())
+        Cuerpo.velocity = new Vector2(MovimientosJugador * VelocidadMovimiento, Cuerpo.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space) && EnSuelo() && PuedeSaltar)
+            ValorSalto += 0.1f;
+
+        if (Input.GetKeyDown(KeyCode.Space) && EnSuelo() && PuedeSaltar)
+        {
+            Cuerpo.velocity = new Vector2(0.0f, Cuerpo.velocity.y);
+        }
+
+        if (ValorSalto >= 20f && EnSuelo())
+        {
+            TiempoX = MovimientosJugador * VelocidadMovimiento;
+            TiempoY = ValorSalto;
+            Cuerpo.velocity = new Vector2(TiempoX, TiempoY);
+            Invoke("ReinicioSalto", 0.2f);
+        }
+        if (Input.GetKeyUp(KeyCode.Space)) 
+        {
+            if (EnSuelo())
+            {
+                Cuerpo.velocity = new Vector2(MovimientosJugador * VelocidadMovimiento, ValorSalto);
+                ValorSalto = 0.0f;
+            }
+
+            PuedeSaltar = true;
+        }
+    }
+
+    private void ReinicioSalto()
+    {
+        PuedeSaltar = false;
+        ValorSalto = 0;
+    }
+
+    private bool EnSuelo()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(Colision.bounds.center, Colision.bounds.size, 0, Vector2.down, 0.1f, SuperficieLayer);
+        return raycastHit.collider != null;
     }
 }
